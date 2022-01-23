f10367835c55154fe3356494368e360034731282
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2022-01-23 16:05:44 +0100

    unit tests for new image interpolation

--- test/src/test_image.cpp
+++ test/src/test_image.cpp
@@ -101,5 +101,45 @@ TEST(Image, ImageInterpolateGrad) {
       [&](const Eigen::Vector2d& x) {
         return Eigen::Matrix<double, 1, 1>(img.interp<double>(pd + x));
       },
-      Eigen::Vector2d::Zero(), 1);
+      Eigen::Vector2d::Zero(),
+      1);  // only works with eps=1 with this gradient interpolation
+}
+
+TEST(Image, ImageInterpolateGradBilinearExact) {
+  Eigen::Vector2i offset(231, 123);
+
+  basalt::ManagedImage<uint16_t> img(640, 480);
+  setImageData(img.ptr, img.size());
+
+  Eigen::Vector2d pd = offset.cast<double>() + Eigen::Vector2d(0.4, 0.34345);
+
+  Eigen::Vector3d val_grad = img.interpGradBilinearExact<double>(pd);
+  Eigen::Matrix<double, 1, 2> J_x = val_grad.tail<2>();
+
+  test_jacobian(
+      "d_res_d_x", J_x,
+      [&](const Eigen::Vector2d& x) {
+        return Eigen::Matrix<double, 1, 1>(img.interp<double>(pd + x));
+      },
+      Eigen::Vector2d::Zero(), 1e-4);
+}
+
+TEST(Image, ImageInterpolateGradCubicSplines) {
+  Eigen::Vector2i offset(231, 123);
+
+  basalt::ManagedImage<uint16_t> img(640, 480);
+  setImageData(img.ptr, img.size());
+
+  Eigen::Vector2d pd = offset.cast<double>() + Eigen::Vector2d(0.4, 0.34345);
+
+  Eigen::Vector3d val_grad = img.interpGradCubicSplines<double>(pd);
+  Eigen::Matrix<double, 1, 2> J_x = val_grad.tail<2>();
+
+  test_jacobian(
+      "d_res_d_x", J_x,
+      [&](const Eigen::Vector2d& x) {
+        return Eigen::Matrix<double, 1, 1>(
+            img.interpCubicSplines<double>(pd + x));
+      },
+      Eigen::Vector2d::Zero(), 1e-4);
 }
